72b036ba4e194cf0c5a0f28c993d9189dc08e8cc
amingst <114528063+amingsttc@users.noreply.github.com>
2026-03-30 14:22:56 -0400

    add StoreApiController

---
+++ Merch/Combined/Controllers/StoreApiController.cs
@@ -0,0 +1,52 @@
+﻿using IT.WebServices.Helpers;
+using IT.WebServices.Merch.Combined.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT.WebServices.Merch.Combined.Controllers
+{
+    [AllowAnonymous]
+    [Route("/api/settings/merch/stores")]
+    [ApiController]
+    public class StoreApiController : Controller
+    {
+        private readonly SettingsHelper settings;
+        private readonly ILogger log;
+
+        public StoreApiController(SettingsHelper settings, ILogger<StoreApiController> log)
+        {
+            this.settings = settings;
+            this.log = log;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("")]
+        public async Task<IActionResult> GetStores()
+        {
+            var stores = new List<PublicStore>();
+            var owner = settings.Owner.Merch.Shopify;
+
+            foreach (var store in owner.Stores)
+            {
+                Guid.TryParse(store.InternalStoreID, out var storeID);
+                if (storeID == Guid.Empty)
+                {
+                    continue;
+                }
+                var pubStore = new PublicStore
+                {
+                    StoreId = storeID,
+                    StoreName = store.StoreName,
+                };
+
+                stores.Add(pubStore);
+            }
+
+            return Ok(stores);
+        }
+    }
+}
---
+++ Merch/Combined/Models/PublicStore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT.WebServices.Merch.Combined.Models
+{
+    public class PublicStore
+    {
+        public Guid StoreId { get; set; } = Guid.Empty;
+        public string StoreName { get; set; } = string.Empty;
+    }
+}
